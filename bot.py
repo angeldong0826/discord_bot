cffa42e8f5444a1d284cf0b27d074a439ebbabca
angeldong0826 <adong6@illinois.edu>
2021-06-10 00:24:44 -0500

    updated sensitive information file

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
-from os import name
-import re
-from discord.enums import UserFlags
+# from os import name
+# import re
+# from discord.enums import UserFlags
 from discord.ext import commands
-import random
 import psycopg2
 import os
 import spotipy
@@ -10,54 +9,34 @@ import sys
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
-#load dotenv for sensitive information purposes
-load_dotenv('.env')
+load_dotenv('.env') #load dotenv for sensitive information purposes
 
 #connect to database
 try:
     conn = psycopg2.connect(
-        dbname="spotifybot",
-        user="postgres",
-        password="Jeremiah29:11",
-        host="localhost",
-        port="5432"
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASSWORD'),
+        host=os.getenv('HOST'),
+        port=os.getenv('PORT')
     )
     print("Connected to database.")
 except:
-    print ("Unable to connect to database")
+    print ("Unable to connect to database.")
 
-cur = conn.cursor() #make cursor for establish connection
+cur = conn.cursor() #make cursor to establish connection
+
+#connect to spotify api
+cid = os.getenv('SPOTIFY_CID')
+secret = os.getenv('SPOTIFY_SECRET')
+
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 #initialize bot
 bot = commands.Bot(command_prefix='!')
 
-#idea feature
-@bot.command(name="idea", help="Get a side project idea")
-async def idea(ctx):
-    await ctx.send("Ideas are hard")
-    await ctx.send("Worry not, I think you should...")
-
-    topics = ['chat bot', 'cli', 'game', 'web bot', 'browser extention', 'api', 'web interface']
-    areas = ['note taking', 'social life', 'physical fitness', 'mental health', 'pet care']
-
-    idea = f'Create a new {random.choice(topics)} that helps with {random.choice(areas)}! :slight_smile:'
-    await ctx.send(idea)
-
-#calculator feature
-@bot.command(name="calc", help="Perform a calculation where fn is either +,-,*, or /")
-async def calc(ctx, x: float, fn: str, y: float):
-    if fn == '+':
-        await ctx.send(x + y)
-    elif fn == '-':
-        await ctx.send(x - y)
-    elif fn == '*':
-        await ctx.send(x * y)
-    elif fn == '/':
-        await ctx.send(x / y)
-    else:
-        await ctx.send("We only support 4 function operations")
-
-#register feature
+#register command
 @bot.command(name = "login")
 async def login(ctx):
 
@@ -85,11 +64,10 @@ async def login(ctx):
     await(ctx.send("username: " + ctx.author.name))
     await(ctx.send("discord id: " + str(ctx.author.id)))
     # print(cur.rowcount, "records inserted.")
-    
-    # close connection
-    conn.close()
 
-# cur.copy_to(sys.stdout, 'artists', sep='\t')
+    cur.execute("Commit")
+    conn.close() #close connection
+
 
 #add favorite artist feature
 @bot.command(name="add", help="Add your favorite artists")
@@ -141,9 +119,8 @@ async def add(ctx, artist: str):
     await(ctx.send("Artist successfully favored."))
     #rows_affected=cur.rowcount
     #print(str(rows_affected) + " records inserted.")
-
-    #close connection
-    conn.close()
+    
+    conn.close() #close connection
 
 @bot.command(name="displayartists", help="Display your list of favorite artists")
 async def displayartists(ctx):
@@ -160,16 +137,11 @@ async def displayfavorites(ctx):
     results = cur.fetchall()
     for i in results:
         await(ctx.send(i))
+    
+#TODO delete command
 
 @bot.command(name="new", help="Look for new song release by artist")
 async def new(ctx, artist: str):
-
-    cid = os.getenv('SPOTIFY_CID')
-    secret = os.getenv('SPOTIFY_SECRET')
-
-    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
     results = sp.search(q=artist)
 
     for idx, track in enumerate(results['tracks']['items']):
@@ -184,17 +156,30 @@ async def new(ctx, artist: str):
     #         await ctx.send(new_release['albums']['items'][i]['external_urls']['spotify']) # return corresponding artist spotify link
 
 
-# for i in range(0, 1000, 50):
+    # for i in range(0, 1000, 50):
 
-#     song_count = 50
-#     new_release = sp.new_releases(None, song_count, i)
+    #     song_count = 50
+    #     new_release = sp.new_releases(None, song_count, i)
 
-#     for j in range(song_count):
-#         print(new_release['albums']['items'][j]['name'])
-#         # print(i+j)
+    #     for j in range(song_count):
+    #         print(new_release['albums']['items'][j]['name'])
+    #         # print(i+j)
 
+#calculator command
+@bot.command(name="calc", help="Perform a calculation where fn is either +,-,*, or /")
+async def calc(ctx, x: float, fn: str, y: float):
+    if fn == '+':
+        await ctx.send(x + y)
+    elif fn == '-':
+        await ctx.send(x - y)
+    elif fn == '*':
+        await ctx.send(x * y)
+    elif fn == '/':
+        await ctx.send(x / y)
+    else:
+        await ctx.send("We only support 4 function operations")
 
-# make sure to create a token file (in real life use env variables)
+#opening personal token file
 with open("BOT_TOKEN.txt", "r") as token_file:
     TOKEN = token_file.read()
     print("Token file read.\n")
